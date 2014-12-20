3635ff421ae920a36bc5ef0682eb5b7e4cd487ec
cuoool <chea.bonnak@gmail.com>
2014-12-20 22:16:56 +0700

    Insert record in industry table

--- app/database/seeds/IndustryTableSeeder.php
+++ app/database/seeds/IndustryTableSeeder.php
@@ -7,31 +7,91 @@ class IndustryTableSeeder extends Seeder {
 	 * @return void
 	 */
 	public function run() {
-		DB::table ( 'industry' )->truncate ();
+		DB::table ( 'industries' )->truncate ();
 		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
+				'name' => 'Advertising / Media / Publishing' 
 		] );		
 		Industry::create ( [ 
-				'name' => 'Airline' 
-		] );
+				'name' => 'Architecture / Construction' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services'
-		] );
+				'name' => 'Banking / Financial' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
-		] );
+				'name' => 'Clothing / Garment' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
-		] );
+				'name' => 'Cosmetics / Beauty' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
-		] );
+				'name' => 'Education' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
-		] );
+				'name' => 'Information Technology' 
+		] );		
 		Industry::create ( [ 
-				'name' => 'Accounting/Audit/Tax Services' 
-		] );
+				'name' => 'Insurance' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Energy / Oil / Gasoline' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Entertainment' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Food &amp; Beverages' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Freight / Shipping / Delivery' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'General Business Services' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Hospital / Pharmacy' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Industrial Machinery' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Hotel / Accommodation' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Human Resources / Consultant' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Manufacturing' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'NGO / Social Services' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Other' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Property Management' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Real Estate' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Recruiting Services' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Sports &amp; Recreation' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Telecommunication' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Tourism' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Trading' 
+		] );		
+		Industry::create ( [ 
+				'name' => 'Wholesale / Retail' 
+		] );	
 	}
 }
