6542ab2f19931a424efbd70f4fba7e9069f3329e
cuoool <chea.bonnak@gmail.com>
2014-12-20 22:14:25 +0700

    Update browse industry dynamically

--- app/views/layouts/body.blade.php
+++ app/views/layouts/body.blade.php
@@ -194,124 +194,13 @@
 									</div>
 									<div class="tab-pane" id="industry">
 										<ul>
-
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-2-advertising+media+publishing">Advertising
-														/ Media / Publishing</a></span> <span
-												class="counter badge"><em>1</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-5-architecture+construction">Architecture
-														/ Construction</a></span> <span class="counter badge"><em>9</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-7-banking+financial">Banking
-														/ Financial</a></span> <span class="counter badge"><em>9</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-11-clothing+garment">Clothing
-														/ Garment</a></span> <span class="counter badge"><em>18</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-12-cosmetics+beauty">Cosmetics
-														/ Beauty</a></span> <span class="counter badge"><em>3</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-13-education">Education</a></span>
-												<span class="counter badge"><em>32</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-43-energy+oil+gasoline">Energy
-														/ Oil / Gasoline</a></span> <span class="counter badge"><em>5</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-15-entertainment">Entertainment</a></span>
-												<span class="counter badge"><em>4</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-16-food-&amp;-beverages">Food
-														&amp; Beverages</a></span> <span class="counter badge"><em>15</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-17-freight+shipping+delivery">Freight
-														/ Shipping / Delivery</a></span> <span
-												class="counter badge"><em>4</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-18-general-business-services">General
-														Business Services</a></span> <span class="counter badge"><em>10</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-19-hospital+pharmacy">Hospital
-														/ Pharmacy</a></span> <span class="counter badge"><em>3</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-44-hotel+accommodation">Hotel
-														/ Accommodation</a></span> <span class="counter badge"><em>15</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-20-human-resources+consultant">Human
-														Resources / Consultant</a></span> <span
-												class="counter badge"><em>49</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-21-industrial-machinery">Industrial
-														Machinery</a></span> <span class="counter badge"><em>5</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-22-information-technology">Information
-														Technology</a></span> <span class="counter badge"><em>4</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-23-insurance">Insurance</a></span>
-												<span class="counter badge"><em>6</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-27-manufacturing">Manufacturing</a></span>
-												<span class="counter badge"><em>11</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-28-ngo+social-services">NGO
-														/ Social Services</a></span> <span class="counter badge"><em>10</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-42-other">Other</a></span>
-												<span class="counter badge"><em>35</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-31-property-management">Property
-														Management</a></span> <span class="counter badge"><em>10</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-32-real-estate">Real
-														Estate</a></span> <span class="counter badge"><em>5</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-33-recruiting-services">Recruiting
-														Services</a></span> <span class="counter badge"><em>331</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-35-sports-&amp;-recreation">Sports
-														&amp; Recreation</a></span> <span class="counter badge"><em>9</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-37-telecommunication">Telecommunication</a></span>
-												<span class="counter badge"><em>5</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-38-tourism">Tourism</a></span>
-												<span class="counter badge"><em>5</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-39-trading">Trading</a></span>
-												<span class="counter badge"><em>2</em></span></li>
-
-											<li><span class="text"><a
-													href="https://camupjob.com/industry-41-wholesale+retail">Wholesale
-														/ Retail</a></span> <span class="counter badge"><em>5</em></span></li>
-
-
+											@foreach($industries as $industry)
+												<li>
+													<span class="text">
+													<a href="https://camupjob.com/industry-2-advertising+media+publishing">{{$industry->name}}</a></span> 
+													<span class="counter badge"><em>1</em></span>
+												</li>
+											@endforeach
 										</ul>
 									</div>
 									<div class="tab-pane" id="location">
