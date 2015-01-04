f2ca18bac5e519d1268d426dc93f7df747817e0a
Bonnak <chea.bonnak@gmail.com>
2015-01-04 08:01:49 +0700

    Update candidate

--- app/models/Candidate.php
+++ app/models/Candidate.php
@@ -8,40 +8,23 @@ class Candidate extends Eloquent
 	{
 		$locale = App::getLocale();
 		
-		if ($locale === 'en')
-		{
-			$profile = DB::table('candidates AS c')
-					->select(DB::raw(
-						"surname,
-						name,
-						(SELECT sex FROM gender WHERE id = c.sex LIMIT 1)sex,
-						date_of_birth,
-						(SELECT status FROM marital WHERE id = c.marital_status LIMIT 1) marital_status,
-						(SELECT nationality FROM countries WHERE id = c.nationality LIMIT 1) nationality,
-						phone_number,
-						(SELECT name FROM locations WHERE id = c.residence LIMIT 1) residence,
-						address"
-					))
-					->where('id', '=', $id)
-					->first();
-		}
-		else
-		{
-			$profile = DB::table('candidates AS c')
+		
+		$profile = DB::table('candidates AS c')
 				->select(DB::raw(
-						"surname,
-						name,
-						(SELECT sex_{$locale} FROM gender WHERE id = c.sex LIMIT 1) sex,
-						date_of_birth,
-						(SELECT status_{$locale} FROM marital WHERE id = c.marital_status LIMIT 1) marital_status,
-						(SELECT nationality FROM countries WHERE id = c.nationality LIMIT 1) nationality,
-						phone_number,
-						(SELECT name FROM locations WHERE id = c.residence LIMIT 1) residence,
-						address"
+					"surname,
+					name,
+					sex,
+					date_of_birth,
+					marital_id,
+					nationality_id,
+					(SELECT nationality FROM countries WHERE id = c.nationality_id LIMIT 1) nationality,
+					phone_number,
+					residence_id,
+					(SELECT name FROM locations WHERE id = c.residence_id LIMIT 1) residence,
+					address"
 				))
 				->where('id', '=', $id)
 				->first();
-		}
 		
 		return $profile;
 	}
