1f5437062fe2d8a75d8db7815ae243e7c443929d
Bonnak <chea.bonnak@gmail.com>
2015-01-04 09:32:30 +0700

    Update model Func, Salary

--- app/models/Func.php
+++ app/models/Func.php
@@ -4,4 +4,10 @@ class Func extends Eloquent
 {
 	protected $table = 'functions';
 	protected $fillable = ['name', 'created_at', 'updated_at'];
+	
+	public static function getFunctions()
+	{
+		return DB::table('functions')->select(['id', 'name'])
+									->get();
+	}
 }
--- app/models/Salary.php
+++ app/models/Salary.php
@@ -4,4 +4,15 @@ class Salary extends Eloquent
 {
 	protected $table = 'salaries';
 	protected $fillable = ['min', 'max', 'created_at', 'updated_at'];
+	
+	public static function getSalaries()
+	{
+		return DB::table('salaries')
+					->select(DB::raw(
+						"id,
+						(CASE WHEN min is null THEN 'Below' ELSE min END) min,
+						(CASE WHEN max is null THEN 'Up' ELSE max END) max"
+					))
+					->get();
+	}
 }
