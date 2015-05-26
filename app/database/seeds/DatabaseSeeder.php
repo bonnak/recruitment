2fafb15a0067240a99fd2a88ce0a7771e850f91c
vearak <thachvearak@gmail.com>
2015-05-26 13:12:38 +0700

    call function appliedJobListSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UserTableSeeder');
 		$this->call('CandidateSeeder');
 		$this->call('EmployerSeeder');
+		$this->call('AppliedJobListSeeder');
 	}
 
 }
