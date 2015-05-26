eb0a74a548d3f23078284e487317079d5f49de26
vearak <thachvearak@gmail.com>
2015-05-26 13:10:51 +0700

    db:seed for applied job list

---
+++ app/database/seeds/AppliedJobListSeeder.php
@@ -0,0 +1,31 @@
+<?php
+class AppliedJobListSeeder extends Seeder {
+	
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		/**
+		 * *** Candidate table ****
+		 */
+		DB::table ( 'applied_job_list' )->truncate ();
+		
+		AppliedJobList::create ( [ 
+				'job_id' => 1,
+				'cv_id'  => 1,
+				'status' => 1
+		] );
+		AppliedJobList::create ( [ 
+				'job_id' => 2,
+				'cv_id'  => 2,
+				'status' => 2
+		] );
+		AppliedJobList::create ( [ 
+				'job_id' => 3,
+				'cv_id'  => 3,
+				'status' => 3
+		] );
+	}
+}
