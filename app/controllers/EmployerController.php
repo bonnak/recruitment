a18a6dd7bcd07f4222a93d5aec01933490b3f840
vearak <thachvearak@gmail.com>
2015-05-26 13:09:50 +0700

    update Employer Controller for post job validator

--- app/controllers/EmployerController.php
+++ app/controllers/EmployerController.php
@@ -42,23 +42,10 @@ class EmployerController extends BaseController
 	//
 	public function postJobPost($emp_id)
 	{
-		// // Get all the input data.
-		// $title = htmlentities(\Input::get('input-job-title'));
-		// $job_description = htmlentities(\Input::get('input-job-description'));
-		// $salary_range = htmlentities(\Input::get('input-salary-range'));
-		// $gender = htmlentities(\Input::get('input-gender'));
-		// $hiring = htmlentities(\Input::get('input-hiring'));
-		// $qualification_id = htmlentities(\Input::get('input-qualification'));
-		// $min_year_exp = htmlentities(\Input::get('input-min-year-exp'));
-		// $age_range = htmlentities(\Input::get('input-age-range'));
-		// $function_id = htmlentities(\Input::get('input-function'));
-		// $industry_id  = htmlentities(\Input::get('input-industry'));
-		// $closing_date = htmlentities(\Input::get('input-closing-date'));
-		// Validate required fields.
+		
 		$validator = \Validator::make(Input::all(), [
-				'input-job-title' => 'required',
-				'input-job-description' => 'required',
-				'input-closing-date'=>'required'
+			'input-job-title' => 'required',
+			'input-job-description' => 'required'				
 		],
 		[
 			'required' => 'Field is reqired.'
@@ -68,52 +55,30 @@ class EmployerController extends BaseController
 		if($validator->fails())
 		{
 			return Redirect::back()->withErrors($validator);
-		}
-			//------------------------------------------------
-		$job = new \Job;
-		$job->employer_id 		= $emp_id;
-		$job->title 			= \Input::get('input-job-title');
-		$job->job_description 	= \Input::get('input-job-description');
-		$job->salary_range 		= \Input::get('input-salary-range');
-		$job->gender 			= \Input::get('input-gender');
-		$job->hiring 			= \Input::get('input-hiring');
-		$job->qualification_id 	= \Input::get('input-qualification');
-		$job->min_year_exp 		= \Input::get('input-min-year-exp');
-		$job->age_range 		= \Input::get('input-age-range');
-		$job->function_id 		= \Input::get('input-function');
-		$job->industry_id 		= \Input::get('input-industry');
-		$job->closing_date 		= \Input::get('input-closing-date');
-		$job->published_date 	= date("Y-m-d");
-		$job->status 			= isset($_GET['publish']) ? 1 : 0;
+		}else{
+			
+			$job = new \Job;
+			$job->employer_id 		= $emp_id;
+			$job->title 			= \Input::get('input-job-title');
+			$job->job_description 	= \Input::get('input-job-description');
+			$job->salary_range 		= \Input::get('input-salary-range');
+			$job->gender 			= \Input::get('input-gender');
+			$job->hiring 			= \Input::get('input-hiring');
+			$job->qualification_id 	= \Input::get('input-qualification');
+			$job->min_year_exp 		= \Input::get('input-min-year-exp');
+			$job->age_range 		= \Input::get('input-age-range');
+			$job->function_id 		= \Input::get('input-function');
+			$job->industry_id 		= \Input::get('input-industry');
+			$job->closing_date 		= \Input::get('input-closing-date');
+			$job->published_date 	= date("Y-m-d");
+			$job->status 			= isset($_GET['publish']) ? 1 : 0;
 
-		if($job ->save()){
-			return Redirect::route('employer.job-post', Auth::user()->id)
-							->with("message","Job has inserted Success!");
+			if($job ->save()){
+				return Redirect::route('employer.job-post', Auth::user()->id)
+						->with("message","Job has inserted Success!");
+			}
 		}
-		//-----------------------------		
 		
-		// // Store a new job post to the database.		
-		// $job = new \Job;		
-		// $job->employer_id = $emp_id;
-		// $job->title = $title; 
-		// $job->job_description = $job_description;
-		// $job->salary_range = !empty($salary_range) ? $salary_range : null;
-		// $job->gender = !empty($gender) ? $gender : null;
-		// $job->hiring = !empty($hiring) ? $hiring : null;
-		// $job->qualification_id = !empty($qualification_id) ? $qualification_id : null;
-		// $job->min_year_exp = !empty($min_year_exp) ? $min_year_exp : null;
-		// $job->age_range = !empty($age_range) ? $age_range : null;
-		// $job->function_id = !empty($function_id) ? $function_id : null;
-		// $job->industry_id = !empty($industry_id) ? $industry_id : null;
-		// $job->status = Input::get('public_check') == null ? 1 : 0;
-		// $job->published_date = date("Y-m-d");
-		// $job->closing_date = !empty($closing_date) ? $closing_date : null;
-		// //if $job equal true it will save
-		// if($job->save())
-		// {
-		// 	return Redirect::route('employer.job-post', Auth::user()->id)
-		// 					->with("message","Job has inserted Success!");;
-		// }
 	}
 	//**
 	//Get Job for Delete .
@@ -184,7 +149,15 @@ class EmployerController extends BaseController
 					->with('show_job', Job::find($id));	
 	}
 	//**
-	// download file 
+	// applied job list 
 	//
+	public function AppleidJob(){
+		$applied_job = AppliedJobList::getapplyList();
+		return View::make('employer.applied-job')
+					->with('applied', $applied_job);
+
+
+
+	}
 
 }
