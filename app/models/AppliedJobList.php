80349625cebe42fb93460f852d7fdc56ab4528c9
vearak <thachvearak@gmail.com>
2015-05-26 13:13:58 +0700

    create applied job list for show list

---
+++ app/models/AppliedJobList.php
@@ -0,0 +1,26 @@
+<?php
+
+class AppliedJobList extends Eloquent 
+{
+	protected $table = 'applied_job_list';
+
+	public static function getapplyList()
+	{		
+		
+		$this_table_nme = with(new static)->getTable();
+
+		return DB::table("{$this_table_nme} as app")
+					->join('jobs', 'app.job_id', '=', 'jobs.id')
+					->join('cv', 'app.cv_id', '=', 'cv.id')
+					->select(DB::raw(
+						"
+						app.id,
+						jobs.title job_title,
+						(SELECT (surname) FROM candidates WHERE id = cv.candidate_id LIMIT 1) candidate_name,
+						app.created_at applied_date,
+						app.status
+						"
+						))
+					->get();
+	}
+}
