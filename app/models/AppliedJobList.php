3c72d1740c597895705019bf0dce536b13a33190
vearak <thachvearak@gmail.com>
2015-05-28 08:31:46 +0700

    update applied_job_list model

--- app/models/AppliedJobList.php
+++ app/models/AppliedJobList.php
@@ -4,23 +4,26 @@ class AppliedJobList extends Eloquent
 {
 	protected $table = 'applied_job_list';
 
-	public static function getapplyList()
+	public static function getapplyList($filter)
 	{		
 		
 		$this_table_nme = with(new static)->getTable();
 
-		return DB::table("{$this_table_nme} as app")
+		return  DB::table("{$this_table_nme} as app")
 					->join('jobs', 'app.job_id', '=', 'jobs.id')
 					->join('cv', 'app.cv_id', '=', 'cv.id')
 					->select(DB::raw(
 						"
 						app.id,
 						jobs.title job_title,
-						(SELECT (surname) FROM candidates WHERE id = cv.candidate_id LIMIT 1) candidate_name,
+						(SELECT surname FROM candidates WHERE id = cv.candidate_id LIMIT 1) candidate_name,
 						app.created_at applied_date,
 						app.status
 						"
 						))
-					->get();
+					->Orwhere('app.status', 'LIKE', '%'. $filter. '%')
+					->paginate(3);
+					
 	}
+	
 }
